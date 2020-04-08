aebdaa28ae2f700bbb872905f6cd4bc99105b42a
Akina Ohira <56147732+akinaohira@users.noreply.github.com>
2020-04-08 17:51:08 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict'
+{
+
+  const width = 10;
+  const height = 15;
+  
+  let board = document.getElementById('board');
+
+  function makeBoard() {
+    for(let x = 0; x < width; x++){
+      let column = document.createElement('div');
+      board.appendChild(column);
+      for(let y = 0; y < height ; y++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        if(x ===0 && y===0 ||
+          x === 0 && y === 1 ||
+          x === 0 && y === 2 ||
+          x === 0 && y === 3){
+          cell.style.background = "pink";
+        } 
+        column.appendChild(cell);      
+      }
+    }
+
+  }
+
+  makeBoard();
+
+}
