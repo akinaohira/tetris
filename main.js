d4e0cb5428051e6eede8e00ba75c77730973129d
Akina Ohira <56147732+akinaohira@users.noreply.github.com>
2020-04-08 21:38:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
   
   let board = document.getElementById('board');
 
+  // make board 
   function makeBoard() {
     for(let x = 0; x < width; x++){
       let column = document.createElement('div');
@@ -13,7 +14,8 @@
       for(let y = 0; y < height ; y++){
         let cell = document.createElement('div');
         cell.classList.add('cell')
-        if(x ===0 && y===0 ||
+        cell.textContent = `${x} : ${y+1}`;
+              if(x ===0 && y===0 ||
           x === 0 && y === 1 ||
           x === 0 && y === 2 ||
           x === 0 && y === 3){
